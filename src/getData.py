872b818349bd3ade1e2f46a6cd64a65547d7a21a
Daniel Díaz <daniel16antonio@gmail.com>
2021-03-18 12:55:26 -0500

    finished import_data function of src/getData file, now can split data in 2 subsets

--- src/getData.py
+++ src/getData.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, DefaultDict
+from collections import defaultdict
 
 import tensorflow as tf
 import numpy as pn
@@ -65,18 +66,63 @@ def get_classes() -> List:
             classes.append(cl)
     return classes
 
-def import_data(setType:str, split:bool=False, splitSize:int=None) -> Union[tf.Tensor, Tuple]:
+def get_count_per_class(path:tf.Tensor)-> DefaultDict[str, int]:
+    """
+    Get a count of items per class
+    parameter(s):
+        path (tf.Tensor): Tensor with the path of the images.
+    returns:
+        countDict (dict): dict with the classe as key and its count as value.
+    """
+    countDict = defaultdict(lambda : 0)
+    for imagePath in path:
+        imageClass = tf.strings.split(imagePath, sep=os.path.sep)[-2]
+        imageClass = imageClass.numpy().decode("ascii")
+        countDict[imageClass]+=1
+    return countDict
+
+def get_splitted_path(setPath:tf.Tensor, countDict:DefaultDict) -> tf.Tensor:
+    """
+    Splitting dataset in 2.
+    parameter(s):
+        setPath (tf.Tensor): path of each image in the dataset
+        countDict (DefaultDict): dict with number of image per class for the new set
+    return:
+        firstSet (tf.Tensor) = principal dataSet
+        secondSet (tf.Tensor) = second dataSet
+    """
+    firstSet, SecondSet = [],[]
+    for imagePath in setPath:
+        imageClass = tf.strings.split(imagePath, sep=os.path.sep)[-2].numpy().decode("ascii")
+        countDict[imageClass] -= 1
+        if countDict[imageClass] > 0 :
+            SecondSet.append(imagePath)
+        else:
+            firstSet.append(imagePath)
+    firstSet, SecondSet = tf.convert_to_tensor(firstSet, tf.string), tf.convert_to_tensor(SecondSet, tf.string)
+    return firstSet, SecondSet
+
+
+def import_data(setType:str, split:bool=False, splitSize:float=.2) -> Union[tf.Tensor, Tuple[tf.Tensor, tf.Tensor]]:
     """
     Import image from directory
     Parameter(s):
         setType (str): Name of subdiretory inside of dirPath (train/test/val).
         split (bool): Whether or not to split the dataset.
-        splitSize (int): If split equals True, this is the percentage of split to create another set. 
+        splitSize (int): If split equals True, this is the percentage to create another set. 
     Returns:
         dataset (tuple(tf.Tensor(tf.Tensor,tf.Tensor), tf.Tensor(tf.Tensor,tf.Tensor))|tf.Tensor(tf.Tensor,tf.Tensor)): dataset itself with their labels
     """
     path = os.path.join(INPUTPATH, setType, '*', '*')
     setPath = tf.data.Dataset.list_files(path)
-    dataset = setPath.map(get_image)
+    if split:
+        countDict = get_count_per_class(setPath)
+        countDict.update((k, int(v*splitSize)) for k,v in countDict.items())
+        firstSetPath, secondSetPath = get_splitted_path(setPath, countDict)
 
-    return dataset
+        firstSetPath = tf.data.Dataset.from_tensor_slices(firstSetPath).map(get_image)
+        secondSetPath = tf.data.Dataset.from_tensor_slices(secondSetPath).map(get_image)
+        return firstSetPath, secondSetPath
+    else:
+        dataset = setPath.map(get_image)
+        return setPath
